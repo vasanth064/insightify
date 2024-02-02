0e8ac97303250c7c3d5e5e2aaa6ab3a1a54d0ea3
Vasanth V V <vasanthvdev@gmail.com>
2024-02-02 12:50:41 +0530

    added remote client for db

--- configs/mongodb.js
+++ configs/mongodb.js
@@ -11,14 +11,12 @@ const {
   MONGODB_NAME,
 } = process.env;
 
-const database = `mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_NAME}?authSource=admin`;
+const database = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@cluster0.g4swqbg.mongodb.net/?retryWrites=true&w=majority`;
 
 const connectDB = async () => {
   try {
     mongoose.set('strictQuery', false);
-    mongoose.connect(database, {
-      useNewUrlParser: true,
-    });
+    mongoose.connect(database, {});
     console.log('MongoDB Connection Success');
   } catch (err) {
     console.error('MongoDB Connection Failed => ', err);
