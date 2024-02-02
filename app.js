892f398f4059480475d89c7773c1e7623d21d6e4
Sanjeevrcs <sanjeevkrcs@gmail.com>
2024-02-02 10:29:12 +0530

    prettier | configured

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-import express from 'express';
-import AppError from './helpers/appError.js';
-import errorHandler from './helpers/errorHandler.js';
-import { rateLimit } from 'express-rate-limit';
-import helmet from 'helmet';
-import monogoSanitize from 'express-mongo-sanitize';
-import xss from 'xss-clean';
-import hpp from 'hpp';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
+import express from "express";
+import AppError from "./helpers/appError.js";
+import errorHandler from "./helpers/errorHandler.js";
+import { rateLimit } from "express-rate-limit";
+import helmet from "helmet";
+import monogoSanitize from "express-mongo-sanitize";
+import xss from "xss-clean";
+import hpp from "hpp";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
@@ -15,23 +15,23 @@ app.use(
   cors({
     origin: true,
     credentials: true,
-  })
+  }),
 );
-app.options('*', cors());
+app.options("*", cors());
 
 app.use(
   helmet({
     crossOriginResourcePolicy: false,
-  })
+  }),
 );
 
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
-  message: 'Too many request from this IP, Please Try again later',
+  message: "Too many request from this IP, Please Try again later",
 });
 
-app.use('/api', limiter);
+app.use("/api", limiter);
 
 app.use(monogoSanitize());
 
@@ -40,23 +40,23 @@ app.use(xss());
 app.use(
   hpp({
     whitelist: [],
-  })
+  }),
 );
 
 app.use(cookieParser());
-app.use(express.json({ limit: '10kb' }));
+app.use(express.json({ limit: "10kb" }));
 
 // app.use('/public', protectedRoute, express.static('public'));
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.status(200).json({
-    status: 'success',
+    status: "success",
   });
 });
 
 // app.use('/api/v1/users', userRouter);
 
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server !`));
 });
 
